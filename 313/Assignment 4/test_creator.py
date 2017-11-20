aa911fc83b2ddbea37b94c748a2292ebd0cd8384
andrewletz <andrewletz@gmail.com>
2017-11-19 21:47:05 -0800

    313 red-black updates

--- 313/Assignment 4/test_creator.py
+++ 313/Assignment 4/test_creator.py
@@ -7,21 +7,25 @@ def driver():
 	num_set = set()
 	for n in range(1000000):
 		choice = random.random()
-		if (choice < 0.2):
-			num = str(random.randint(-100000, 100000))
+		if (choice <= 0.2):
+			num = str(random.randint(-1000000, 1000000))
 			if (num not in num_set):
-				f.write("insert " + str(random.randint(-100000, 100000)) + "\n")
-			num_set.add(num)
-		elif (choice < 0.4):
-			f.write("remove " + str(random.randint(-100000, 100000)) + "\n")
-		elif (choice < 0.6):
-			f.write("search " + str(random.randint(-100000, 100000)) + "\n")
-		elif (choice < 0.8):
+				f.write("insert " + num + "\n")
+				num_set.add(num)
+			else:
+				f.write("min" + "\n")
+		elif (choice <= 0.4):
+			f.write("remove " + str(random.randint(-1000000, 1000000)) + "\n")
+		elif (choice <= 0.6):
+			f.write("search " + str(random.randint(-1000000, 1000000)) + "\n")
+		elif (choice <= 0.8):
 			f.write("max" + "\n")
-		elif (choice < 0.99):
-			f.write("min" + "\n")
 		elif (choice <= 1):
-			f.write("print" + "\n")
+			f.write("min" + "\n")
+		else:
+			f.write("min" + "\n")
+		#elif (choice <= 1):
+		#	f.write("print" + "\n")
 
 if __name__ == "__main__":
 	driver()
