71bee55c3c4ac8cda708299c361ad32ff56d61bf
AndrewLetz <andrewletz@gmail.com>
2018-01-15 18:19:06 -0800

    Moved assignment 1 to folder and added comment

--- 315/Assignment1/addMultNum.py
+++ 315/Assignment1/addMultNum.py
@@ -4,7 +4,7 @@ import fileinput
 def driver():
 	counter = 0
 	for line in fileinput.input():
-		if (counter > 0):
+		if (counter > 0): # using for each so we can skip first line
 			in_data = line.strip().split()
 			first, second = int(in_data[0]), int(in_data[1])
 			addition = first + second
