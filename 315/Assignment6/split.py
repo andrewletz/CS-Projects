86b51a506ef514a3d474a973cf308d7fd5f87856
andrewletz <andrewletz@gmail.com>
2018-03-07 21:27:58 -0800

    Finished 315 assignment 6 iterative + fixed lag

--- 315/Assignment6/split.py
+++ 315/Assignment6/split.py
@@ -1,9 +1,37 @@
 import fileinput
 
-words = []
+words = {}
 
 def dict(w):
 	return w in words;
+
+iterDPrint = {}
+def iterSplit(s, n):
+	a = [i for i in range(n)]
+	global iterD
+	iterD = {key: False for key in a}
+
+	for i in range(n - 1, -1, -1): # go backwards
+		if (iterD[i] == False) and dict(s[i:n]):
+			iterD[i] = True
+			iterDPrint[i] = s[i:n]
+
+		if iterD[i] == True:
+			if i == 0:  # reached the end so if true it is splittable
+				return True
+
+			for j in range(i - 1, -1, -1):
+
+				if (iterD[j] == False) and dict(s[j:i]):
+					iterD[j] = True
+					iterDPrint[j] = s[j:i]
+
+				if (j == 0) and (iterD[j] == True): # reached the end so if true it is splittable
+					return True
+		
+
+	return False
+
 
 memo = {}
 # returns true if possible to correctly add spaces to x_i, x_i+1... x_n
@@ -15,17 +43,15 @@ def split(s, n, i):
 		return memo[i]
 	boo = False
 	j = i
-	for j in range(n):
+	for j in range(n): #
 		boo = boo or (dict(s[i:j+1]) and split(s, n, j + 1))
 	memo[i] = boo
 	return boo
 
-
 def driver():
 	with open("diction10k.txt") as f:
 		for line in f:
-			words.append(line.strip())
-
+			words[line.strip()] = True
 
 	counter = 0
 	for line in fileinput.input():
@@ -36,6 +62,7 @@ def driver():
 			string = line.strip()
 			print("\nphrase number:", counter)
 			print(string, "\n")
+
 
 			print("memoized attempt:")
 			canBeSplit = split(string, len(string), 0)
@@ -61,6 +88,26 @@ def driver():
 				print(string[sortedKeys[i]:len(string)], end='')
 				print("")
 
+
+			print("\niterative attempt:")
+			canBeSplitIter = iterSplit(string, len(string))
+			if (canBeSplitIter):
+				print("YES, can be split")
+			else:
+				print("NO, cannot be split")
+
+			if (canBeSplitIter):
+				prStr = ""
+				i = 0
+				while i < len(string):
+					word = iterDPrint[i]
+					prStr += word
+					prStr += " "
+					i += len(word)
+				print(prStr)
+
+			iterD.clear()
+			iterDPrint.clear()
 			memo.clear()
 
  
